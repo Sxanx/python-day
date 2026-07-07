c47df471ba76ca3e632f4831bee34467f6a0a43f
Sxanx <nirunkunchua@gmail.com>
2026-07-07 18:21:41 +0700

    lecture-2

---
+++ lecture-02/testInput.py
@@ -0,0 +1,3 @@
+fristName = input("Enter your frist name: ")
+lastName = input("Enter your last name: ")
+print("Hello ,"+ fristName + " " + lastName + "!")
---
+++ lecture-02/testInput2.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+income = float(input("Enter your income: "))
+
+print ("THe information your input are:")
+print("Name: "+ name)
+print("Age: " + age)
+print("Income: ", format(income, "12,.2f"))
